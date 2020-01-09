e6c044cab619cb8679265e1376e1b3da6882d17b
Anna Panas <annapanas0906@gmail.com>
2020-01-09 13:45:23 -0500

    fixed future hours to correct color

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 
 // displaying current day once the page loads using $(document).ready(function()
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-  // using moment() method to update current date using moment.js library
-  var currentDay = moment().format('dddd, MMMM Do');
+    // using moment() method to update current date using moment.js library
+    var currentDay = moment().format('dddd, MMMM Do');
 
-  // creating an h2 alement and asigning it's value to currentDay value
-  var h2 = $("<h2>");
-  h2.text(currentDay);
-  $("#day").append(h2);
-  h2.addClass("col-12 offset-4");
+    // creating an h2 alement and asigning it's value to currentDay value
+    var h2 = $("<h2>");
+    h2.text(currentDay);
+    $("#day").append(h2);
+    h2.addClass("col-12 offset-4");
 
 });
 
@@ -27,29 +27,30 @@ function renderEvents() {
 
     for (var i = 0; i < 9; i++) {
         var now = hour[i];
-         
+        var testing = 0;
+
         var row = $("<div>");
         row.addClass("row");
 
-    // input area for hours
+        // input area for hours
         var hourBlock = $("<div>");
         hourBlock.addClass("col-1 hour-block");
         hourBlock.text(hour[i]);
 
-    // input area for user to write down the events
+        // input area for user to write down the events
         var textBlock = $("<div>");
         textBlock.addClass("col-6 text-block");
         var textarea = $("<textarea>");
         textarea.value = event;
         textBlock.append(textarea);
 
-    // creating save buttons    
+        // creating save buttons    
         var saveBlock = $("<div>");
         saveBlock.addClass("col-1 save-block");
         var button = $("<button>");
         button.text("Save");
         saveBlock.append(button);
-        
+
         row.append(hourBlock);
         row.append(textBlock);
         row.append(saveBlock);
@@ -57,53 +58,41 @@ function renderEvents() {
         $("#put-calendar-here").append(row);
         row.addClass("col-12 offset-2");
 
-        var currentHour = moment().format('h A');
+        var currentHour = moment().format('hA');
         console.log(currentHour);
-    
-    
-    if (currentHour === hour[i]){
-        $("<textarea>").css("background-color","red");
-    }
-    
-    if(currentHour < hour[i]){
-        $("<textarea>").css("background-color","gray");
-    }
-    
-    else{
-        $("<textarea>").css("background-color","green");
-    }
+
+        //  each hour should be color coded to reflect whether the time slot is in the past, the present, or the future.
+        // This will change depending on the time of day.   
+
+
+
+        if ( hour[i] < currentHour ) {
+            textarea.addClass("past");
+        }
+
+        else if (currentHour === hour[i]) {
+            textarea.removeClass("past");
+            textarea.addClass("current");
+        }
+
+
+
+        else {
+            textarea.removeClass("past");
+            textarea.removeClass("current");
+            textarea.addClass("future");
+        }
     }
 }
 
 //-------------------------------------------------------------------------------------------------//
 
-//  each hour should be color coded to reflect whether the time slot is in the past, the present, or the future.
-// This will change depending on the time of day.
+// Clicking on the save button will store the time and user input in localStorage
 
-// $(document).ready(function(){
-
-// // using moment() method to check for current hour using moment.js library
-//     var currentHour = moment().format('h A');
-//     console.log(currentHour);
-
-
-// if (currentHour === hour[i]){
-//     $("<textarea>").css("background-color","red");
-// }
-
-// if(currentHour < hour[i]){
-//     $("<textarea>").css("background-color","gray");
-// }
-
-// else{
-//     $("<textarea>").css("background-color","green");
-// }
-// renderEvents();
-// });
 
 //---------------------------------------------------------------------------------------------------//
 
 
 
-// Clicking on the save button will store the time and user input in localStorage
+
 
