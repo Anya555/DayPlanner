013f9d6aca82968a22d16a39b1c3e708941b03c1
Anna Panas <annapanas0906@gmail.com>
2020-01-10 12:39:56 -0500

    adding local storage

--- main.js
+++ main.js
@@ -4,7 +4,11 @@
 $(document).ready(function () {
 
     // using moment() method to update current date using moment.js library
-    var currentDay = moment().format('dddd, MMMM Do');
+    var currentDayObj = moment();
+    var currentDay = currentDayObj.format('dddd, MMMM Do');
+    currentDayObj.set('hour', 9);
+
+    console.log(currentDayObj);
 
     // creating an h2 alement and asigning it's value to currentDay value
     var h2 = $("<h2>");
@@ -12,109 +16,119 @@ $(document).ready(function () {
     $("#day").append(h2);
     h2.addClass("col-12 offset-4");
 
-});
+    //-------------------------------------------------------------------------------------------------//
 
-//-------------------------------------------------------------------------------------------------------//
-
-// an array of standard business hours (9 a.m. to 5 p.m.). 
-var hour = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
-var otherHour = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
-
-renderEvents();
-
-// function that creates an input area for each hour, event from 9am to 5 pm (total hours = 9) and save buttons
-function renderEvents() {
-
-
-    for (var i = 0; i < 9; i++) {
-        var now = hour[i];
-        var testing = 0;
-
-        var row = $("<div>");
-        row.addClass("row");
-
-        // input area for hours
-        var hourBlock = $("<div>");
-        hourBlock.addClass("col-1 hour-block");
-        hourBlock.text(hour[i]);
-
-        // input area for user to write down the events
-        var textBlock = $("<div>");
-        textBlock.addClass("col-6 text-block");
-        var textarea = $("<textarea>");
-        textarea.value = event;
-        textBlock.append(textarea);
-
-        // creating save buttons    
-        var saveBlock = $("<div>");
-        saveBlock.addClass("col-1 save-block");
-        var button = $("<button>");
-        button.text("Save");
-        saveBlock.append(button);
-
-        row.append(hourBlock);
-        row.append(textBlock);
-        row.append(saveBlock);
-
-        $("#put-calendar-here").append(row);
-        row.addClass("col-12 offset-2");
-
-        var currentHour = moment().format('hA');
-        // var currentHour = moment().format('H');  // 24 hour time format
-        console.log(currentHour);
-        console.log(hour[i]);
-
-        //  each hour should be color coded to reflect whether the time slot is in the past, the present, or the future.
-        // This will change depending on the time of day.   
-
-
-        // You are comparing two string values here "9AM" and the current time (right now its "1PM").
-        // We need to convert the two times to Integer values here if we want to compare them in an if() statement.
-
-        if ( hour[i] < currentHour ) {
-            console.log("true");
-            textarea.removeClass("future");
-            textarea.removeClass("current");
-            textarea.addClass("past");
-        } else if (currentHour === hour[i]) {
-            textarea.removeClass("past");
-            textarea.addClass("current");
-            textarea.removeClass("future");
-        } else {
-            textarea.removeClass("past");
-            textarea.removeClass("current");
-            textarea.addClass("future");
-            console.log(textarea);
-        }
-
-        // var convertArrayHours = otherHour[i].split(" ");
-        // var convertCurrentHour = currentHour.split(" ");
-        // console.log(convertArrayHours[0]);
-        // console.log(convertCurrentHour);
-
-        // if ( convertArrayHours < convertCurrentHour ) {
-        //     textarea.addClass("past");
-        // } else if (convertCurrentHour === convertArrayHours) {
-        //     textarea.removeClass("past");
-        //     textarea.addClass("current");
-        //     textarea.removeClass("future");
-        // } else {
-        //     textarea.removeClass("past");
-        //     textarea.removeClass("current");
-        //     textarea.addClass("future");
-        //     console.log(textarea);
-        // }
+    var storedData = {
+        "9am": "",
+        "10am": "",
+        "11am": "",
+        "12pm": "",
+        "1pm": "",
+        "2pm": "",
+        "3pm": "",
+        "4pm": "",
+        "5pm": ""
     }
-}
 
-//-------------------------------------------------------------------------------------------------//
+    // TODO: Grab data from localStorage and assign it to storedData variable
+    var getFromLocalStorage = function(){
+        var data = JSON.parse(localStorage.getItem("storedData"));
+    }
 
-// Clicking on the save button will store the time and user input in localStorage
+    // TODO: Save storedData to localStorage
+   
+    // save-btn.addEventListener("click", saveToLocalStorage);
+    var saveToLocalStorage = function(){
+        localStorage.setItem("storedData", JSON.stringify(data));
+    }
+
+    
+    console.log(storedData["9am"]);
+
+    // an array of standard business hours (9 a.m. to 5 p.m.). 
+   
+    var hour = [
+        currentDayObj,
+        currentDayObj.clone().add(1, "hour"), 
+        currentDayObj.clone().add(2, "hour"),
+        currentDayObj.clone().add(3, "hour"),
+        currentDayObj.clone().add(4, "hour"),
+        currentDayObj.clone().add(5, "hour"),
+        currentDayObj.clone().add(6, "hour"),
+        currentDayObj.clone().add(7, "hour"),
+        currentDayObj.clone().add(8, "hour")
+    ];
+
+    // ------------------------------------------------------------------------------------------------//
+
+    renderEvents();
+
+    // creating an input area for each hour, event from 9am to 5 pm (total hours = 9) and save buttons
+    function renderEvents() {
+        for (var i = 0; i < hour.length; i++) {
+            var now = hour[i];
+            var timeText = hour[i].format("ha");
+
+            console.log(hour[i])
+            var row = $("<div>");
+            row.addClass("row");
+
+            // input area for hours
+            var hourBlock = $("<div>");
+            hourBlock.addClass("col-1 hour-block");
+            hourBlock.text(timeText);
+
+            // input area for user to write down the events
+            var textBlock = $("<div>");
+            textBlock.addClass("col-6 text-block");
+          
+            // Determine color
+            var bgColor = getTextBgColor(hour[i]);
+            textBlock.css("backgroundColor", bgColor);
+            var textarea = $("<textarea>");
+            textarea.attr("data-time", timeText);
+            textarea.val(storedData[timeText]);
+            textBlock.append(textarea);
+
+            // creating save buttons    
+            var saveBlock = $("<div>");
+            saveBlock.addClass("col-1 save-block");
+            var button = $("<button>");
+            button.text("Save");
+            button.addClass("save-btn");
+            button.attr("data-time", timeText);
+            saveBlock.append(button);
+
+            row.append(hourBlock);
+            row.append(textBlock);
+            row.append(saveBlock);
+
+            $("#put-calendar-here").append(row);
+            row.addClass("col-12 offset-2");
+
+        }
+    }
 
 
-//---------------------------------------------------------------------------------------------------//
-
-
-
-
+    // Each timeblock is color coded to indicate whether it is in a past, present, or future hour.
+    function getTextBgColor(hour){
+        var now = moment();
+        console.log(now.format("H") + "|" + hour.format("H"));
+        var color = "";
+        // future
+        if( hour.format("H") < now.format("H")){
+            console.log('less');
+            color = "green";
+        // current
+        } else if ( hour.format("H") === now.format("H")){
+            console.log('current');
+            color="red";
+        // past
+        } else {
+            console.log('greater');
+            color="gray";
+        }
+        return color;
+    }
+});
 
